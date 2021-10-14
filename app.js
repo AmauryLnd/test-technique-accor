278d078ec1fefce4bd4f52ba15f1e73935f90890
Amaury <amaury.linde@gmail.com>
2021-10-14 23:08:46 +0200

    Added findHotelsNearby function

--- app.js
+++ app.js
@@ -3,9 +3,25 @@ const hotelService = require('./services/hotel-service');
 const priceService = require('./services/price-service');
 const helper = require('./services/helper');
 
-function findHotelsNearby(lat, lng, radius) {
-    // TODO implement me
-	return [];
+function findHotelsNearby (lat, lng, radius = 2000) {
+
+	const hotelsNearby = [];
+	hotelService.getHotels().forEach((hotel) => {
+		const distance = helper.distance(lat, lng, hotel.latitude, hotel.longitude)
+
+		if (distance <= radius) {
+			nearbyHotels.push({
+				ridCode: hotel.ridCode,
+				countryCode: hotel.countryCode,
+				localRating: hotel.localRating,
+				address: hotel.address,
+				commercialName: hotel.commercialName,
+				distance: distance.toFixed(0)
+			})
+		}
+	})
+
+	return JSON.stringify(hotelsNearby);
 }
 
 function findHotelNearbyWithBestOffer(lat, lng, radius, date) {
